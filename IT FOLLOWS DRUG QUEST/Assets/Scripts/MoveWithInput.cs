9b307f84499bcd12cdd284f8374574009646ed2d
jessica.boyd <jessica.boyd@innogames.com>
2019-06-26 00:16:49 +0200

    health variable

--- IT FOLLOWS DRUG QUEST/Assets/Scripts/MoveWithInput.cs
+++ IT FOLLOWS DRUG QUEST/Assets/Scripts/MoveWithInput.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 public class MoveWithInput : MonoBehaviour
 {
 	public float speed = 5f;
+    public int maxHealth = 5;
+    int currentHealth;
 
-	public KeyCode leftButton = KeyCode.A;
+    Rigidbody2D rigidbody2d;
+
+    public KeyCode leftButton = KeyCode.A;
 	public KeyCode rightButton = KeyCode.D;
 	public KeyCode upButton = KeyCode.W;
 	public KeyCode downButton = KeyCode.S;
+
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+    }
 
     void Update()
     {
@@ -27,5 +39,11 @@ public class MoveWithInput : MonoBehaviour
 			transform.Translate(0f, -amount, 0f, Space.World);
 	}
 
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
 
