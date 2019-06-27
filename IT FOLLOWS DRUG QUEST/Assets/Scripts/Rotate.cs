015330f4f7efbeb6370ecae29b40055c7f4b63a6
Martin Paradis <martinpa@unity3d.com>
2019-06-27 09:57:25 -0400

    Added simple auto-rotation

---
+++ IT FOLLOWS DRUG QUEST/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Rotate : MonoBehaviour
+{
+    public float degreesPerSecond = 360.0f;
+    private SpriteRenderer spriteRenderer;
+    
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        var rotation = degreesPerSecond * Time.deltaTime;
+        transform.Rotate(Vector3.forward, rotation);
+    }
+}
